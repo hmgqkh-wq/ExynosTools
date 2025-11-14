eb09692b163e2d8f7f21efa8c59f082cf6b810be
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-15 00:09:28 +0330

    Update xeno_log.h

--- include/xeno_log.h
+++ include/xeno_log.h
@@ -6,8 +6,7 @@
 #include <stdarg.h>
 #include "logging.h"
 
-/* Do not redefine XENO_LOGE / XENO_LOGI if logging.h already defines them.
-   logging.h in your tree defines these macros; this header prefers existing definitions. */
+/* Prefer logging.h definitions; only define if missing */
 #ifndef XENO_LOGE
 #define XENO_LOGE(...) logging_error(__VA_ARGS__)
 #endif
