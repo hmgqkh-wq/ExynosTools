c30affc195d759e590b2a97f16fe84b18f0cbd56
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-14 16:00:35 +0330

    Update bc_emulate.h

--- src/bc_emulate.h
+++ src/bc_emulate.h
@@ -1,23 +1,29 @@
-// SPDX-License-Identifier: MIT
-// ExynosTools: BC decode interface
+#ifndef BC_EMULATE_H
+#define BC_EMULATE_H
 
-#pragma once
-#include <stdint.h>
 #include <vulkan/vulkan.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 typedef enum {
-    BC1 = 0,
-    BC3,
-    BC4,
-    BC5,
-    BC6H,
-    BC7,
-    XENO_BC_FORMAT_COUNT
+    XENO_BC1 = 0,
+    XENO_BC3 = 1,
+    XENO_BC4 = 2,
+    XENO_BC5 = 3,
+    XENO_BC6H = 4,
+    XENO_BC7 = 5,
+    XENO_BC_FORMAT_COUNT = 6
 } XenoBCFormat;
+
+typedef struct {
+    VkDevice device;
+    VkPhysicalDevice physicalDevice;
+    VkPipelineCache pipelineCache;
+    VkDescriptorSetLayout descriptorSetLayout;
+    VkPipelineLayout pipelineLayout;
+    VkDescriptorPool descriptorPool;
+    VkPipeline pipelines[XENO_BC_FORMAT_COUNT];
+    uint32_t workgroup_size;
+    int initialized;
+} XenoBCContext;
 
 typedef struct {
     uint32_t baseMipLevel;
@@ -26,34 +32,9 @@ typedef struct {
     uint32_t arrayLayerCount;
 } XenoSubresourceRange;
 
-typedef struct XenoBCContext {
-    VkDevice device;
-    VkPhysicalDevice physicalDevice;
-    VkPipelineCache pipelineCache;
-    VkDescriptorSetLayout descriptorSetLayout;
-    VkPipelineLayout pipelineLayout;
-    VkDescriptorPool descriptorPool;
-    VkPipeline pipelines[XENO_BC_FORMAT_COUNT];
-    VkImageView dstView;
-    uint32_t workgroup_size;
-    int initialized;
-
-    // Optional bindless path
-    VkDescriptorSetLayout bindlessLayout;
-    VkDescriptorPool bindlessPool;
-    VkDescriptorSet bindlessSet;
-    int hasDescriptorIndexing;
-    int hasRayTracing;
-} XenoBCContext;
-
 XenoBCContext* xeno_bc_create_context(VkDevice device, VkPhysicalDevice phys);
 void xeno_bc_destroy_context(XenoBCContext* ctx);
+VkResult xeno_bc_decode_image(VkCommandBuffer cmd, XenoBCContext* ctx, VkBuffer src_bc, VkImage dst_rgba,
+                              XenoBCFormat format, VkExtent3D extent, XenoSubresourceRange subres);
 
-VkResult xeno_bc_decode_image(VkCommandBuffer cmd, XenoBCContext* ctx,
-                              VkBuffer src_bc, VkImage dst_rgba,
-                              XenoBCFormat format, VkExtent3D extent,
-                              XenoSubresourceRange subres);
-
-#ifdef __cplusplus
-}
 #endif
