728cca7b0692e099bb0b2eb9e92a7d2ed71f3267
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-15 00:43:53 +0330

    Create create_common_layouts_and_pipelines.c

---
+++ src/create_common_layouts_and_pipelines.c
@@ -0,0 +1,9 @@
+#include "xeno_log.h"
+#include "logging.h"
+
+/* minimal stub for configure to succeed when included in CMake sources */
+int create_common_stub(void)
+{
+    XENO_LOGI("create_common_stub");
+    return 0;
+}
