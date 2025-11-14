0b0e4b73e68363ac5c7a519430a704f1da2e0ca8
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-14 09:42:44 +0330

    Update generate_shader_header.cmake

--- generate_shader_header.cmake
+++ generate_shader_header.cmake
@@ -12,11 +12,7 @@ file(TO_CMAKE_PATH "${HEADER_FILE}" __hdr_out)
 
 if("${SPIRV_FILE}" STREQUAL "__MISSING__" OR NOT EXISTS "${__spv_in}")
   message(STATUS "generate_shader_header: SPIR-V missing, producing fallback ${__hdr_out}")
-  file(WRITE "${__hdr_out}" "#pragma once
-#include <stdint.h>
-static const uint32_t ${VAR_NAME}[] = { 0x00000000 };
-static const unsigned int ${VAR_NAME}_len = 4;
-")
+  file(WRITE "${__hdr_out}" "#pragma once\n#include <stdint.h>\nstatic const uint32_t ${VAR_NAME}[] = { 0x00000000 };\nstatic const unsigned int ${VAR_NAME}_len = 4;\n")
   return()
 endif()
 
@@ -32,21 +28,21 @@ foreach(i RANGE 0 ${_num_words} 1)
     math(EXPR idx "${off} + ${b}")
     if(idx LESS _len_bytes)
       string(SUBSTRING "${_BIN_DATA}" ${idx} 1 _byte)
-      execute_process(COMMAND /bin/printf "%d" "'${_byte}"
+      execute_process(
+        COMMAND /bin/printf "%d" "'${_byte}"
         OUTPUT_VARIABLE _bval
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
       math(EXPR _w "${_w} + (${_bval} << (${b} * 8))")
     endif()
   endforeach()
-  execute_process(COMMAND /bin/printf "%08x" "${_w}"
+  execute_process(
+    COMMAND /bin/printf "%08x" "${_w}"
     OUTPUT_VARIABLE _hex
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
   string(APPEND _words "0x${_hex}, ")
 endforeach()
 
-file(WRITE "${__hdr_out}" "#pragma once
-#include <stdint.h>
-static const uint32_t ${VAR_NAME}[] = { ${_words} };
-static const unsigned int ${VAR_NAME}_len = ${_len_bytes};
-")
-message(STATUS "Generated ${__hdr_out} (${_num_words} words, ${_len_bytes} bytes)")
+file(WRITE "${__hdr_out}" "#pragma once\n#include <stdint.h>\nstatic const uint32_t ${VAR_NAME}[] = { ${_words} };\nstatic const unsigned int ${VAR_NAME}_len = ${_len_bytes};\n")
+message(STATUS "Generated header ${__hdr_out} (${_num_words} words, ${_len_bytes} bytes).")
