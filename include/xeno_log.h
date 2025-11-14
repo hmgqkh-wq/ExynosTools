9221d7137e05e83adcff5cbc9c18be05d05d90c0
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-14 23:53:30 +0330

    Update xeno_log.h

--- include/xeno_log.h
+++ include/xeno_log.h
@@ -6,23 +6,21 @@
 #include <stdarg.h>
 #include "logging.h"
 
+/* Do not redefine XENO_LOGE / XENO_LOGI if logging.h already defines them.
+   logging.h in your tree defines these macros; this header prefers existing definitions. */
+#ifndef XENO_LOGE
 #define XENO_LOGE(...) logging_error(__VA_ARGS__)
+#endif
+
+#ifndef XENO_LOGI
 #define XENO_LOGI(...) logging_info(__VA_ARGS__)
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 FILE *xeno_log_stream(void);
-
-static inline void xeno_log_stream_printf(FILE *stream, const char *fmt, ...)
-{
-    (void)stream;
-    va_list ap;
-    va_start(ap, fmt);
-    logging_info(fmt, ap);
-    va_end(ap);
-}
 
 #ifdef __cplusplus
 }
