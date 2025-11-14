3c776ab975986e35215cddfd0a9477c2024dbace
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-14 17:53:00 +0330

    Update vrs.c

--- src/drivers/xclipse/vrs.c
+++ src/drivers/xclipse/vrs.c
@@ -1,6 +1,6 @@
 #include <vulkan/vulkan.h>
 
 void apply_vrs(VkCommandBuffer cmd, VkExtent2D extent) {
-  VkFragmentShadingRateKHR rate = {2, 2};  // 2x2 shading for low-detail, FPS boost
-  vkCmdSetFragmentShadingRateKHR(cmd, &rate, NULL);
+    VkFragmentShadingRateKHR rate = {2, 2};  // 2x2 shading for low-detail, FPS boost
+    vkCmdSetFragmentShadingRateKHR(cmd, &rate, NULL);
 }
