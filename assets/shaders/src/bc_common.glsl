c5cd69abb5e582c8b52112cd257e3d5fb0314b48
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-14 19:04:33 +0330

    Update bc_common.glsl

--- assets/shaders/src/bc_common.glsl
+++ assets/shaders/src/bc_common.glsl
@@ -1,6 +1,6 @@
-#version 450
 #extension GL_GOOGLE_include_directive : enable
 
+// Shared helpers. No #version here (only in .comp files).
 float unpackAlpha(uint packed) {
     return float(packed) / 255.0;
 }
