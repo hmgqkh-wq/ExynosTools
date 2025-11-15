28145a4788e80c4c53f14e8ef63192c6b86725f7
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-15 12:24:26 +0330

    Update async.c

--- src/drivers/xclipse/async.c
+++ src/drivers/xclipse/async.c
@@ -1,43 +1,18 @@
-#include "xeno_log.h"
+/*
+  src/drivers/xclipse/async.c
+  Async helpers for Xclipse 940.
+*/
 #include "logging.h"
-#include <vulkan/vulkan.h>
-#include <stdint.h>
-#include <stddef.h>
+#include <stdatomic.h>
 
-VkResult create_async_queue(VkDevice device, VkQueue* queue, VkCommandPool* pool)
-{
-    if (!device || !queue || !pool) return VK_ERROR_INITIALIZATION_FAILED;
+static _Atomic int async_counter = 0;
 
-    uint32_t queueFamilyIndex = 0;
-    vkGetDeviceQueue(device, queueFamilyIndex, 0, queue);
-
-    VkCommandPoolCreateInfo poolInfo = {
-        .sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
-        .flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT,
-        .queueFamilyIndex = queueFamilyIndex
-    };
-    return vkCreateCommandPool(device, &poolInfo, NULL, pool);
+void async_submit_begin(void) {
+    atomic_fetch_add(&async_counter, 1);
+    logging_info("Async submit begin (counter=%d)", async_counter);
 }
 
-VkResult async_decode_submit(VkQueue queue, VkCommandBuffer cmd)
-{
-    if (queue == VK_NULL_HANDLE || cmd == VK_NULL_HANDLE) return VK_ERROR_INITIALIZATION_FAILED;
-
-    VkSubmitInfo submitInfo = {
-        .sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
-        .pNext = NULL,
-        .waitSemaphoreCount = 0,
-        .pWaitSemaphores = NULL,
-        .pWaitDstStageMask = NULL,
-        .commandBufferCount = 1,
-        .pCommandBuffers = &cmd,
-        .signalSemaphoreCount = 0,
-        .pSignalSemaphores = NULL
-    };
-
-    VkResult res = vkQueueSubmit(queue, 1, &submitInfo, VK_NULL_HANDLE);
-    if (res != VK_SUCCESS) {
-        XENO_LOGE("Async submit failed: %d", res);
-    }
-    return res;
+void async_submit_end(void) {
+    atomic_fetch_sub(&async_counter, 1);
+    logging_info("Async submit end (counter=%d)", async_counter);
 }
