cb11b9d78b2e07c1ea0f843f95ba0c987c6edd9f
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-15 14:56:43 +0330

    Create xeno_wrapper.h

---
+++ include/xeno_wrapper.h
@@ -0,0 +1,28 @@
+// include/xeno_wrapper.h
+#ifndef XENO_WRAPPER_H
+#define XENO_WRAPPER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <vulkan/vulkan.h>
+#include "xeno_bc.h"
+
+/* Public prototypes for xeno wrapper functions used by other modules. */
+VkResult xeno_wrapper_create_device(VkPhysicalDevice physicalDevice,
+                                    const VkDeviceCreateInfo *pCreateInfo,
+                                    const VkAllocationCallbacks *pAllocator,
+                                    VkDevice *pDevice);
+
+void xeno_wrapper_begin_render(VkCommandBuffer commandBuffer,
+                               const VkRenderPassBeginInfo *pRenderPassBeginInfo,
+                               VkSubpassContents contents);
+
+void xeno_wrapper_destroy(struct XenoBCContext *maybe_ctx);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* XENO_WRAPPER_H */
