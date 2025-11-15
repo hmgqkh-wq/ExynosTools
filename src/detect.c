ea99b07bf536a9ea5c0feb8dfa7d9cd293abf3c8
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-15 12:23:39 +0330

    Update detect.c

--- src/detect.c
+++ src/detect.c
@@ -1,7 +1,16 @@
-#include "xeno_log.h"
+/*
+  src/detect.c
+  Device detection helpers.
+*/
+#include "logging.h"
+#include <vulkan/vulkan.h>
 
-int detect_stub(void)
-{
-    XENO_LOGI("detect_stub");
+int detect_xclipse940(VkPhysicalDevice physical) {
+    VkPhysicalDeviceProperties props;
+    vkGetPhysicalDeviceProperties(physical, &props);
+    if (props.deviceID == 0x940) {
+        logging_info("Detected Xclipse 940 GPU");
+        return 1;
+    }
     return 0;
 }
