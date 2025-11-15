f4feae801c5310298eb991ba0d38091910a07e30
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-15 14:34:21 +0330

    Create rt_path.h

---
+++ include/rt_path.h
@@ -0,0 +1,32 @@
+// include/rt_path.h
+#ifndef RT_PATH_H
+#define RT_PATH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <vulkan/vulkan.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/* Exposed helpers for buffer creation/upload and utilities used by other units. */
+VkResult rt_create_buffer_with_memory(VkDevice device, VkPhysicalDevice physical,
+                                      VkDeviceSize size, VkBufferUsageFlags usage,
+                                      VkMemoryPropertyFlags properties,
+                                      VkBuffer *outBuffer, VkDeviceMemory *outMemory);
+
+void rt_destroy_buffer_with_memory(VkDevice device, VkBuffer buffer, VkDeviceMemory memory);
+
+VkResult rt_upload_to_buffer(VkDevice device, VkDeviceMemory memory, VkDeviceSize offset,
+                             const void *data, VkDeviceSize size, VkPhysicalDevice physical);
+
+size_t rt_guess_staging_size(VkDeviceSize requested);
+
+void rt_log_buffer_address(VkDevice device, VkBuffer buffer);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* RT_PATH_H */
