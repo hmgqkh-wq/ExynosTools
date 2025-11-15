74045d211a2a574ec13ef81b40f1ac8462534ca8
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-15 17:33:24 +0330

    Update xeno_log_stream.c

--- src/xeno_log_stream.c
+++ src/xeno_log_stream.c
@@ -1,6 +1,12 @@
-#include "xeno_log.h"
 #include <stdio.h>
 
-FILE *xeno_log_stream(void) {
-    return stderr;
+static FILE* g_log_stream = NULL;
+
+FILE* xeno_log_stream(void) {
+    if (!g_log_stream) g_log_stream = stderr;
+    return g_log_stream;
+}
+
+int xeno_log_enabled_debug(void) {
+    return 1; /* enable debug by default */
 }
