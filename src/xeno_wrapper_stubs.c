b710feb93ba80668f123f9b3b53a4bcde6618a4d
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-16 14:38:21 +0330

    Delete src/xeno_wrapper_stubs.c

--- src/xeno_wrapper_stubs.c
+++
@@ -1,9 +0,0 @@
-#include <vulkan/vulkan.h>
-
-/* Provide default NULL pointers for "original" hooks so the linker is satisfied.
-   Your wrapper should check for NULL before calling these. */
-
-PFN_vkCreateDevice vkCreateDevice_original = NULL;
-PFN_vkCmdBeginRenderPass vkCmdBeginRenderPass_original = NULL;
-
-/* Add more if your code references additional *_original symbols */
